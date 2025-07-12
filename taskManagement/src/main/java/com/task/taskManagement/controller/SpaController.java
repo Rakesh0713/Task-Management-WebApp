e7768ea59f011c0a25d0b8631ee2039381d2a9fc
RAKESH SARAN K <143988491+Rakesh0713@users.noreply.github.com>
2025-07-12 20:04:12 +0530

    updated ui and enpoints

--- taskManagement/src/main/java/com/task/taskManagement/controller/SpaController.java
+++ taskManagement/src/main/java/com/task/taskManagement/controller/SpaController.java
@@ -5,8 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SpaController {
-    @GetMapping({"/", "/{path:^(?!api|static|css|js|images).*$}", "/{path:^(?!api|static|css|js|images).*$}/**"})
-    public String index() {
-        return "add";
+    @GetMapping("/")
+    public String landing() {
+        return "landing";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "dashboard";
+    }
+
+    @GetMapping({"/{path:^(?!api|static|css|js|images).*$}", "/{path:^(?!api|static|css|js|images).*$}/**"})
+    public String fallback() {
+        return "landing";
     }
 } 
